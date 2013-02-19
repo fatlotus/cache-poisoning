0b8074ad584f0b951654414deb1234f4b16b3217
Jeremy Archer <open-source@fatlotus.com>
2013-02-18 23:38:14 -0600

    Add skeletal README.md

--- cache.py
+++ cache.py
@@ -4,9 +4,9 @@ import os
 from google.appengine.ext import db
 
 class Session(db.Model):
-  hits = db.IntegerProperty(default = 0)
+  pages = db.StringListProperty(default = [ ])
 
-def hit(client_id):
+def hit(client_id, referrer):
   try:
     id = int(client_id)
   except ValueError:
@@ -14,9 +14,9 @@ def hit(client_id):
   
   def txn():
     sess = Session.get_by_id(id)
-    sess.hits += 1
+    sess.pages.append(referrer)
     sess.put()
-    return sess.hits
+    return sess.pages
   
   return db.run_in_transaction(txn)
 
@@ -28,7 +28,7 @@ def main():
     print 'Expires: Tue, 15 Nov 1994 12:45:26 GMT'
     print
     
-    print 'Hits: %i' % hit(os.environ['PATH_INFO'][7:])
+    print repr(hit(os.environ['PATH_INFO'][7:], os.environ.get('HTTP_REFERER', '')))
   
   elif os.environ['PATH_INFO'] == '/bug':
     if 'HTTP_IF_MODIFIED_SINCE' in os.environ and false:
@@ -38,13 +38,13 @@ def main():
     sess = Session()
     sess.put()
     
-    print 'Status: 200 Okay'
+    print 'Status: 303 Redirect'
     print 'Content-type: text/html'
     print 'Cache-Control: public, max-age=99936000'
     print 'Last-Modified: Tue, 15 Nov 1994 12:45:26 GMT'
     print 'Expires: Sun, 01 Dec 2030 16:00:00 GMT'
+    print 'Location: /track/%i' % sess.key().id()
     print
-    print '<iframe src="/track/%i"></iframe>' % sess.key().id()
   
   else:
     print 'Status: 404 File Not Found'
