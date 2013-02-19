81cdca77b462f31efb71bd1848e6b27641e3617f
Jeremy Archer <open-source@fatlotus.com>
2013-02-18 23:52:50 -0600

    Improve robustness.

--- cache.py
+++ cache.py
@@ -2,40 +2,38 @@ import datetime
 import logging
 import os
 from google.appengine.ext import db
+import uuid
 
 class Session(db.Model):
   pages = db.StringListProperty(default = [ ])
 
-def hit(client_id, referrer):
-  try:
-    id = int(client_id)
-  except ValueError:
-    return
+def hit(name, referrer):
   
-  def txn():
-    sess = Session.get_by_id(id)
-    sess.pages.append(referrer)
-    sess.put()
-    return sess.pages
+  # Should really be transactional...
   
-  return db.run_in_transaction(txn)
+  sess = Session.get_by_key_name(name)
+  if sess is None:
+    sess = Session(key_name = name)
+  sess.pages.append(referrer)
+  sess.put()
+  return sess.pages
 
 def main():
   if os.environ['PATH_INFO'].startswith('/track/'):
     print 'Status: 200 Okay'
-    print 'Content-type: text/html'
+    print 'Content-type: text/plain'
     print 'Cache-Control: no-cache; must-revalidate'
     print 'Expires: Tue, 15 Nov 1994 12:45:26 GMT'
     print
     
-    print repr(hit(os.environ['PATH_INFO'][7:], os.environ.get('HTTP_REFERER', '')))
+    print "\n".join((hit(os.environ['PATH_INFO'][7:], os.environ.get('HTTP_REFERER', ''))))
   
   elif os.environ['PATH_INFO'] == '/bug':
     if 'HTTP_IF_MODIFIED_SINCE' in os.environ and false:
       print 'Status: 304 Not Modified'
       return
     
-    sess = Session()
+    sess = Session(key_name = 's%s' % uuid.uuid4())
     sess.put()
     
     print 'Status: 303 Redirect'
@@ -43,7 +41,7 @@ def main():
     print 'Cache-Control: public, max-age=99936000'
     print 'Last-Modified: Tue, 15 Nov 1994 12:45:26 GMT'
     print 'Expires: Sun, 01 Dec 2030 16:00:00 GMT'
-    print 'Location: /track/%i' % sess.key().id()
+    print 'Location: /track/%s' % sess.key().name()
     print
   
   else:
